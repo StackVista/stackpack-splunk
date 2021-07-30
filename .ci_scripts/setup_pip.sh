9023387544c504112c97a9ee1cb64200c79f9965
Bram Schuur <bschuur@stackstate.com>
2021-07-30 15:15:20 +0200

    STAC-0: Work on github actions:

--- .ci_scripts/setup_pip.sh
+++ .ci_scripts/setup_pip.sh
@@ -1,4 +1,4 @@
 ## python artifactory dependency
-mkdir ~/.pip/ && touch ~/.pip/pip.conf
+mkdir -p ~/.pip/ && touch ~/.pip/pip.conf
 echo "[global]" > ~/.pip/pip.conf
 echo "extra-index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@artifactory.stackstate.io/artifactory/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
