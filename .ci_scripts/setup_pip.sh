b7ed0494528ccb03046438c8b165dbb7b17f1188
Bram Schuur <bschuur@stackstate.com>
2021-07-30 15:18:39 +0200

    Fix artifactory user

--- .ci_scripts/setup_pip.sh
+++ .ci_scripts/setup_pip.sh
@@ -1,4 +1,4 @@
 ## python artifactory dependency
 mkdir -p ~/.pip/ && touch ~/.pip/pip.conf
 echo "[global]" > ~/.pip/pip.conf
-echo "extra-index-url = https://$ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD@artifactory.stackstate.io/artifactory/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
+echo "extra-index-url = https://${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}@artifactory.stackstate.io/artifactory/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
