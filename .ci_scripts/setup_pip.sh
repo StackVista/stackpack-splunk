b59f5ff05a43f1c5c11595521f76fc9fd549d8aa
Bram Schuur <bschuur@stackstate.com>
2021-07-30 14:14:07 +0200

    Add splunk checks to splunk stackpack

---
+++ .ci_scripts/setup_pip.sh
@@ -0,0 +1,4 @@
+## python artifactory dependency
+mkdir ~/.pip/ && touch ~/.pip/pip.conf
+echo "[global]" > ~/.pip/pip.conf
+echo "extra-index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@artifactory.stackstate.io/artifactory/api/pypi/pypi-local/simple" >> ~/.pip/pip.conf
